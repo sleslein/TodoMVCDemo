ef5bed32681f93e241e079a7ac4d42fb53755e3a
sleslein <shea.leslein@gmail.com>
2017-01-06 21:22:24 -0600

    Add react files and copied files from TODO MVC example

--- TodoMVCDemo/App_Start/BundleConfig.cs
+++ TodoMVCDemo/App_Start/BundleConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Optimization;
+using System.Web.Optimization.React;
 
 namespace TodoMvcDemo
 {
@@ -8,21 +9,27 @@ namespace TodoMvcDemo
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/shared").Include(
+                        "~/Scripts/jquery/jquery-3.1.1.js",
+                        "~/Scripts/bootstrap/bootstrap.js",
+                        "~/Scripts/respond.js"
+                    ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new BabelBundle("~/bundles/todoapp").Include(
+                    "~/Scripts/react/react.js",
+                    "~/Scripts/react/reactDOM.js",
+                    "~/Scripts/director.js",
+                    "~/Scripts/classNames.js",
+                    "~/Scripts/utils.js",
+                    "~/Scripts/todoModel.js",
+                    "~/Scripts/todoItem.jsx",
+                    "~/Scripts/footer.jsx",
+                    "~/Scripts/app.jsx"
+                ));
         }
     }
 }
