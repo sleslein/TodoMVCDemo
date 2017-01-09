8b62fbbe717f4ce2ce678dfc7cf05a2e8d0b044e
sleslein <shea.leslein@gmail.com>
2017-01-09 12:03:48 -0600

    Updates to Polish Simple React Component

--- TodoMVCDemo/App_Start/BundleConfig.cs
+++ TodoMVCDemo/App_Start/BundleConfig.cs
@@ -9,27 +9,15 @@ namespace TodoMvcDemo
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/shared").Include(
-                        "~/Scripts/jquery/jquery-3.1.1.js",
-                        "~/Scripts/bootstrap/bootstrap.js",
-                        "~/Scripts/respond.js"
+            bundles.Add(new BabelBundle("~/bundles/shared").Include(
+                        "~/Scripts/respond.js",
+                        "~/Scripts/react/react.js",
+                        "~/Scripts/react/reactDOM.js",
+                        "~/Scripts/simpleComponent.jsx"
                     ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-
-            bundles.Add(new BabelBundle("~/bundles/todoapp").Include(
-                    "~/Scripts/react/react.js",
-                    "~/Scripts/react/reactDOM.js",
-                    "~/Scripts/director.js",
-                    "~/Scripts/classNames.js",
-                    "~/Scripts/utils.js",
-                    "~/Scripts/todoModel.js",
-                    "~/Scripts/todoItem.jsx",
-                    "~/Scripts/footer.jsx",
-                    "~/Scripts/app.jsx"
-                ));
+                      "~/Content/Site.css"));
         }
     }
 }
---
+++ TodoMVCDemo/Scripts/simpleComponent.jsx
@@ -0,0 +1,29 @@
+﻿const HelloMessage = React.createClass({
+    getInitialState () {
+        return {
+            name: this.props.name
+        };
+    },
+    clickHandler() {        
+        const txtNewName = document.getElementById("newName");
+        const newName = txtNewName.value;
+        txtNewName.value = "";
+        this.setState({ name: newName });
+    },
+    render() {
+        return (
+            <div>
+                <div>Name: {this.state.name}</div>
+                <div>
+                    New Name: <input type="text" id="newName" />
+                    <button type="button" onClick={this.clickHandler}>{this.props.buttonName}</button>
+                </div>
+            </div>
+            );
+        }
+    });
+
+ReactDOM.render(
+    <HelloMessage name="Bala" buttonName="Update Info" />, 
+    document.getElementById('simpleComponent')
+);
